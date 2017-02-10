d60d73960145c1c8e5c532da12f8519802c0eb18
Kay Strobach <info@kay-strobach.de>
2017-02-10 12:10:47 +0100

    [TASK] init

---
+++ Classes/KayStrobach/Gentelella/Controller/StandardController.php
@@ -0,0 +1,23 @@
+<?php
+namespace KayStrobach\Gentelella\Controller;
+
+/*
+ * This file is part of the KayStrobach.Gentelella package.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController
+{
+
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('foos', array(
+            'bar', 'baz'
+        ));
+    }
+
+}
